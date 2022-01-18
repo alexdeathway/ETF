b85640f40caa767d4bf7b7a86f236acfc94d9b9b
alexdeathway <alexdeathway@gmail.com>
2022-01-18 23:40:52 +0530

    event conf

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,6 @@
+from multiprocessing import Event
 from django.contrib import admin
-
+from .models import Event,Ticket
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(Ticket)
--- events/models.py
+++ events/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.
 
 # Create your models here.
 
-class EventModel(models.Model):
+class Event(models.Model):
     name=models.CharField( max_length=20)
+    description=models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
+class Ticket(models.Model):
+    event=models.OneToOneField(Event, on_delete=models.CASCADE)
+    price=models.PositiveIntegerField(max_length=6)
+    ticket_type=
