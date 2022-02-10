f80091ec335150d3557667ef32343cdb4f26d88d
alexdeathway <alexdeathway@gmail.com>
2022-02-10 20:00:07 +0530

    user auth

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.urls import path
 from events.views import (
+                        EventListView,
                         EventCreateView,
                         TicketTypeCreateView,
                         TicketBookingView,
@@ -8,6 +9,7 @@ from events.views import (
 app_name="events"
 
 urlpatterns = [
+    path("",EventListView.as_view(),name='eventlist'),
     path("create/",EventCreateView.as_view(),name='eventcreate'),
     path("ticket-type-create/",TicketTypeCreateView.as_view(),name='tickettypecreate'),
     path("ticket-booking/",TicketBookingView.as_view(),name='ticketbooking'),
--- events/views.py
+++ events/views.py
@@ -3,8 +3,13 @@ from django.urls import reverse
 from events.forms import EventCreationForm ,TicketTypeCreationForm,TicketBookingForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
-                                    CreateView,
+                                ListView,    
+                                CreateView,
+                                DetailView,
                                 )  
+
+class EventListView(ListView):
+    pass
 
 class EventCreateView(LoginRequiredMixin,CreateView):
 
@@ -14,6 +19,9 @@ class EventCreateView(LoginRequiredMixin,CreateView):
     def get_success_url(self):
         reverse('home')
 
+class EventDetailView(DetailView):
+    pass
+
 class TicketTypeCreateView(LoginRequiredMixin,CreateView):
 
     template_name="events/tickettype_create.html"
@@ -22,6 +30,9 @@ class TicketTypeCreateView(LoginRequiredMixin,CreateView):
     def get_success_url(self):
         reverse('home')
 
+
+class TicketListView(ListView):
+    pass
 
 class TicketBookingView(LoginRequiredMixin,CreateView):
 
